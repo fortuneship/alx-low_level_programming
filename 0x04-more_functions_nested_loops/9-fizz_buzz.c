7905625ad5f65024304eb0bff17ca21086fdb4c6
Tunde Babatunde <tunde8983@gmail.com>
2022-09-15 08:04:23 -0700

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,32 +5,38 @@
  * Return: void.
  */
 
-int main(void) {
-    
-    int i;
-    
-    for (i = 1; i <= 100; i++)
-    {
-        if(i % 3 == 0 && i % 5 == 0)
-        {
-            printf("FizzBuzz");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else 
-        {
-            printf("%d", i);
-        }
-        
-        printf(" ");
-        
-    }
-    printf("\n");
-    return (0);
+int main(void)
+{
+
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+
+		i++;
+	}
+	putchar('\n');
+	return (0);
+
 }
